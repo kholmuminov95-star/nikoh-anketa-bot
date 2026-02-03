abadaead0732feb1827d2b742c2c36c02ac1d64c
kholmuminov95-star <kholmuminov95@gmail.com>
2026-02-03 22:50:31 +0500

    Initialize bot with basic start command

---
+++ bot.py
@@ -0,0 +1,13 @@
+from aiogram import Bot, Dispatcher, executor, types
+
+TOKEN = "8219884908:AAHMBf0JP1Cd_w2aGlN_cl_CZmyGoV1gAK4"
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands="start")
+async def start(msg: types.Message):
+    await msg.answer("Bot Railway’da ishlayapti ✅")
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
