3d88d6aeae85f4f57f032bfbe7c829629bff2f99
kholmuminov95-star <kholmuminov95@gmail.com>
2026-02-04 00:36:48 +0500

    Clean up init.py and update usage instructions
    Removed commented-out code and added instructions for file usage.

--- handlers/init.py
+++ handlers/init.py
@@ -1,13 +1,2 @@
-# handlers/__init__.py
-
-# Bu faylni BO'SH qoldiring yoki:
-# from .start import router as start_router
-# from .profile import router as profile_router
-# from .payment import router as payment_router
-# from .request import router as request_router
-# from .admin import router as admin_router
-
-# routers = [start_router, profile_router, payment_router, request_router, admin_router]
-
-# Yoki faqat:
-routers = []  # Bo'sh ro'yxat
+# Bu faylni to'liq o'chirib tashlang yoki:
+# Faqat bo'sh qoldiring
